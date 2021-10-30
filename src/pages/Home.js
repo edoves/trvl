925a538f60e3a9955fd0ebc6dcfa32a515dcf05d
dove <ndoverte11@gmail.com>
2021-10-30 21:31:27 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Cards from "../components/Card";
+import HeroSection from "../components/Hero";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
